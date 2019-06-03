6ba888f7a6463a9416531e3cbc106a35fe3adce8
Willian <willianferrari>
2019-06-03 15:39:11 -0300

    Alteração pastas apks, logs e monkey

--- monkeytest/main.py
+++ monkeytest/main.py
@@ -1,3 +1,108 @@
+# encoding: utf-8
+import csv
+import re
 import os
-myCmd = 'dir > out.txt'
-os.system(myCmd)
+import subprocess
+
+
+from emulador import instalar_apk, abrir_app, desinstalar_apk, ler_apks, abrir_emulator
+from tela import capturar_tela, comparar_imagens
+
+
+
+def main():
+
+    # 1. ler log (stoat e monkey)
+    #f = open('logs/teste.txt', 'r')
+    # f = open('logs/mcmc_all_history_testsuites.txt', 'r')
+
+    #f1 = f.readlines()
+    #abrir_emulator('testAVD_4')
+
+    #2. executar casos de teste no programa original
+    caminho_original = '../apks/original/'
+    apks = ['TippyTipper-debug', 'TranslateActivity-debug', 'arXiv-debug', 'ListLentObjects-debug']
+    apps = ['net.mandaria.tippytipper', 'com.beust.android.translate', 'com.commonsware.android.arXiv', 'de.freewarepoint.whohasmystuff']
+    activities = ['/.activities.TippyTipper', '/.TranslateActivity', '/.arXiv', '/.ListLentObjects']
+    #activities = ['/.activities.TippyTipper', '/TranslateActivity', '/arXiv', '/.activities.TippyTipper']
+    # apks = ['TippyTipper-debug']
+    # apps = ['net.mandaria.tippytipper']
+
+    for i in range(len(apps)):
+        apk = caminho_original + apks[i] + '.apk'
+        instalar_apk(apk)
+        #saida = abrir_app(apps[i] + activities[i])
+        saida = subprocess.call(["timeout", "1h", "adb", "shell", "monkey", "--throttle", "200", "-p", apps, "1000", "-v", "250","--ignore-crashes", "--ignore-timeouts", "--ignore-security-exceptions > monkey.log"])
+        if saida == 0:
+            capturar_tela(apps[i] + '-original.png')
+        else:
+            print 'erro ao abrir app original: ' + apps[i]
+            desinstalar_apk(apps[i])
+            exit(0)
+        desinstalar_apk(apps[i])
+
+    #3. executar casos de teste nos mutantes
+    caminho_mutantes = '../apks/mutantes/'
+    pastas_mutantes = ['tippytipper', 'translate', 'arxiv', 'whohasmystuff'] ### ATENCAO: deve estar na mesma ordem do vetor apps
+
+    for pasta in pastas_mutantes:
+        resultado = []
+        erros = 0
+        apks = ler_apks(caminho_mutantes + pasta)
+        # apks = ['TippyTipper-debug3.apk']
+        for i in range(len(apks)):
+            apk = caminho_mutantes + pasta + '/' + apks[i]
+            instalar_apk(apk)
+            # print apps[i] + '/.activities.TippyTipper'
+            saida = abrir_app(apps[i] + activities[i]) #SplitBill
+            match = re.search(r'\/[\w.-]+-debug\d+.apk', apk)
+            fig_mutante = match.group()[1:-4] + '.png'
+            # print fig_mutante
+
+            if saida == 0:
+
+                subprocess.call(["timeout", "1h", "adb", "shell", "monkey", "--throttle","200", "-p",apps, "1000", "-v","250","--ignore-crashes","--ignore-timeouts","--ignore-security-exceptions > monkey.log"])
+
+                capturar_tela(fig_mutante)
+                similar = comparar_imagens(apps[i] + '-original.png', fig_mutante)
+                resultado.append([apk, similar])
+            else:
+                erros += 1
+                print 'erro ao abrir app mutante: ' + fig_mutante
+                desinstalar_apk(apps[i])
+                exit(0)
+
+            desinstalar_apk(apps[i])
+
+
+
+        #4. resultados
+        dm = 0
+        tm = len(resultado)
+        for r in resultado:
+            print r
+            if not r[1]:
+                dm += 1
+
+
+        print '------------------------ RESULTADOS ------------------------'
+        print resultado
+        m = 0
+        if tm > 0:
+            m = float(dm)/float(tm)
+            print 'taxa de mutacao: ' + str(float(dm)/float(tm))
+        else:
+            print '[==> ATENÇÃO]: Total de mutantes válidos: 0'
+        print 'erros: ' + str(erros)
+
+        with open('resultado.csv', mode='w') as arquivo:
+            employee_writer = csv.writer(arquivo, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            employee_writer.writerow([resultado])
+            employee_writer.writerow([tm, m, erros])
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ monkeytest/tela.py
@@ -0,0 +1,50 @@
+import os
+from time import sleep
+from PIL import Image
+import subprocess
+from image_checker import checkSimilarPictures
+
+STATUS_BAR_CROP_HEIGHT = 80
+SCREEN_CPATURE_DELAY = 0.2
+SCREENSHOTS_DIRECTORY = 'telas/'
+
+
+def comparar_imagens(arquivo1, arquivo2):
+    img1 = os.path.join(SCREENSHOTS_DIRECTORY, arquivo1)
+    img2 = os.path.join(SCREENSHOTS_DIRECTORY, arquivo2)
+    similar, crashed = checkSimilarPictures(img1, img2)
+    return similar
+
+
+def capturar_tela(pic_name):
+    image_path = os.path.join(SCREENSHOTS_DIRECTORY, pic_name)
+    device_path = '/sdcard/%s' % pic_name
+    sleep(SCREEN_CPATURE_DELAY)
+    command = ['adb', 'shell', "screencap -p", device_path]
+    subprocess.call(command)
+    command = ['adb', 'pull', device_path, image_path]
+    subprocess.call(command)
+
+    while not os.path.isfile(image_path):
+        sleep(0.1)
+
+    command = ['adb', 'shell', 'rm', device_path]
+    subprocess.call(command)
+    img = Image.open(image_path)
+    w, h = img.size
+    img.crop((0, STATUS_BAR_CROP_HEIGHT, w, h)).save(image_path)
+    return image_path
+
+# def main():
+#     raw_input("Pressione ENTER para tirar a primeira foto...")
+#     capturar_tela('pic_name1.png', SCREENSHOTS_DIRECTORY)
+#     raw_input("Pressione ENTER para tirar a segunda foto...")
+#     capturar_tela('pic_name2.png', SCREENSHOTS_DIRECTORY)
+#     similar = comparar_imagens('pic_name1.png', 'pic_name2.png')
+#     if similar:
+#         print 'Imagens iguais'
+#     else:
+#         print 'Imagens diferentes'
+#
+# if __name__ == '__main__':
+#     main()
